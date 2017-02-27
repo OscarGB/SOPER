a5bedd5d71075cf1306d30ba26edf1140ed47f29
nagomez97 <nagomez97@gmail.com>
2017-02-27 18:24:58 +0100

    ej8 imcompleto

--- Practica1/ejercicio8.c
+++ Practica1/ejercicio8.c
@@ -14,6 +14,10 @@
 
 int main(int argc, char const *argv[]) {
 	int flag, var, i, j;
+	char *noll[] = {NULL};
+	char path[20] = "/bin/";
+	int status;
+	char *prog[20] = { "programasssssssssssssss", NULL};
 
 	flag = argc - 1;
 
@@ -22,11 +26,18 @@ int main(int argc, char const *argv[]) {
 		var = fork();
 
 		for(i = 1; i < flag; i++){
+
 			if(var == 0) { /*Si es un hijo, ejecuta el exec del flag*/
-				execl(argv[i], 0);
+				strcat(path, argv[i]);
+				execl(path, argv[i], NULL);
+				perror("Error en l\n");
+				exit(EXIT_FAILURE);
 			}
 			else if (var > 0){
-				var = fork();
+				wait(&status);
+				if(i < flag - 1){
+					var = fork();
+				}
 			}
 			else {
 				printf("Error haciendo fork\n");
@@ -41,10 +52,15 @@ int main(int argc, char const *argv[]) {
 
 		for(i = 1; i < flag; i++){
 			if(var == 0) { /*Si es un hijo, ejecuta el exec del flag*/
-				execlp(argv[i], NULL);
+				execlp(argv[i], argv[i], (char *)NULL);
+				perror("Error en lp\n");
+				exit(EXIT_FAILURE);
 			}
 			else if (var > 0){
-				var = fork();
+				wait(&status);
+				if(i < flag - 1){
+					var = fork();
+				}
 			}
 			else {
 				printf("Error haciendo fork\n");
@@ -60,10 +76,17 @@ int main(int argc, char const *argv[]) {
 
 		for(i = 1; i < flag; i++){
 			if(var == 0) { /*Si es un hijo, ejecuta el exec del flag*/
-				execv(argv[i], 0);
+				strcat(path, argv[i]);
+				strcpy(prog[0], path);
+				execv(path, prog);
+				perror("Error en v\n");
+				exit(EXIT_FAILURE);
 			}
 			else if (var > 0){
-				var = fork();
+				wait(&status);
+				if (i < flag-1){
+					var = fork();
+				}
 			}
 			else {
 				printf("Error haciendo fork\n");
@@ -79,10 +102,16 @@ int main(int argc, char const *argv[]) {
 
 		for(i = 1; i < flag; i++){
 			if(var == 0) { /*Si es un hijo, ejecuta el exec del flag*/
-				execvp(argv[i], NULL);
+				strcpy(prog[0], argv[i]);
+				execvp(argv[i], prog);
+				perror("Error en vp\n");
+				exit(EXIT_FAILURE);
 			}
 			else if (var > 0){
-				var = fork();
+				wait(&status);
+				if(i < flag - 1){
+					var = fork();
+				}
 			}
 			else {
 				printf("Error haciendo fork\n");
@@ -96,7 +125,8 @@ int main(int argc, char const *argv[]) {
 		exit(EXIT_FAILURE);
 		return -1;
 	}
-
+	printf("%d", getpid());
+	wait(&status);
 	exit(EXIT_SUCCESS);
 	return 0;
 }
