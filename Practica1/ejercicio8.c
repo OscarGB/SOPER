ef9fbb78ce4cd4afc51b820f68c5aad1fdcc6113
nagomez97 <nagomez97@gmail.com>
2017-02-28 01:19:47 +0100

    ej9 sin acabar

--- Practica1/ejercicio8.c
+++ Practica1/ejercicio8.c
@@ -16,136 +16,136 @@
 #include <sys/types.h>
 
 int main(int argc, char const *argv[]) {
-	int flag, var, i, j;
-	char *noll[] = {NULL};
-	char path[20] = "/bin/";
-	int status;
-	char **prog;
+    int flag, var, i, j;
+    char *noll[] = {NULL};
+    char path[20] = "/bin/";
+    int status;
+    char **prog;
 
-	flag = argc - 1;
+    flag = argc - 1;
 
-	if(strcmp(argv[flag], "-l") == 0) { /*Asumimos que el flag del exec va a 
-										ser el último argumento*/
-		var = fork();
+    if(strcmp(argv[flag], "-l") == 0) { /*Asumimos que el flag del exec va a 
+                                        ser el último argumento*/
+        var = fork();
 
-		for(i = 1; i < flag; i++){
+        for(i = 1; i < flag; i++){
 
-			if(var == 0) { /*Si es un hijo, ejecuta el exec del flag*/
-				strcat(path, argv[i]);
-				execl(path, argv[i], NULL);
-				perror("Error en l\n");
-				exit(EXIT_FAILURE);
-			}
-			else if (var > 0){
-				wait(&status);
-				if(i < flag - 1){
-					var = fork();
-				}
-			}
-			else {
-				printf("Error haciendo fork\n");
-				exit(EXIT_FAILURE);
-			}
-		}
+            if(var == 0) { /*Si es un hijo, ejecuta el exec del flag*/
+                strcat(path, argv[i]);
+                execl(path, argv[i], NULL);
+                perror("Error en l\n");
+                exit(EXIT_FAILURE);
+            }
+            else if (var > 0){
+                wait(&status);
+                if(i < flag - 1){
+                    var = fork();
+                }
+            }
+            else {
+                printf("Error haciendo fork\n");
+                exit(EXIT_FAILURE);
+            }
+        }
 
-	}
-	else if(strcmp(argv[flag], "-lp") == 0) { /*Asumimos que el flag del exec va a 
-										ser el último argumento*/
-		var = fork();
+    }
+    else if(strcmp(argv[flag], "-lp") == 0) { /*Asumimos que el flag del exec va a 
+                                        ser el último argumento*/
+        var = fork();
 
-		for(i = 1; i < flag; i++){
-			if(var == 0) { /*Si es un hijo, ejecuta el exec del flag*/
-				execlp(argv[i], argv[i], (char *)NULL);
-				perror("Error en lp\n");
-				exit(EXIT_FAILURE);
-			}
-			else if (var > 0){
-				wait(&status);
-				if(i < flag - 1){
-					var = fork();
-				}
-			}
-			else {
-				printf("Error haciendo fork\n");
-				exit(EXIT_FAILURE);
-			}
-		}
+        for(i = 1; i < flag; i++){
+            if(var == 0) { /*Si es un hijo, ejecuta el exec del flag*/
+                execlp(argv[i], argv[i], (char *)NULL);
+                perror("Error en lp\n");
+                exit(EXIT_FAILURE);
+            }
+            else if (var > 0){
+                wait(&status);
+                if(i < flag - 1){
+                    var = fork();
+                }
+            }
+            else {
+                printf("Error haciendo fork\n");
+                exit(EXIT_FAILURE);
+            }
+        }
 
-	}
-	else if(strcmp(argv[flag], "-v") == 0) { /*Asumimos que el flag del exec va a 
-										ser el último argumento*/
+    }
+    else if(strcmp(argv[flag], "-v") == 0) { /*Asumimos que el flag del exec va a 
+                                        ser el último argumento*/
 
-		var = fork();
+        var = fork();
 
-		prog = (char **)malloc(sizeof(char*) * COMMANDS); /*array de strings*/
+        prog = (char **)malloc(sizeof(char*) * COMMANDS); /*array de strings*/
 
-		for( i = 0; i < COMMANDS - 1; i++){
-			prog[i] = (char *)malloc(sizeof(char) * COMMANDSIZE);
-		}
-		prog[COMMANDS - 1] = NULL; /*El ultimo tiene que ser NULL*/
+        for( i = 0; i < COMMANDS - 1; i++){
+            prog[i] = (char *)malloc(sizeof(char) * COMMANDSIZE);
+        }
+        prog[COMMANDS - 1] = NULL; /*El ultimo tiene que ser NULL*/
 
-		for(i = 1; i < flag; i++){
-			if(var == 0) { /*Si es un hijo, ejecuta el exec del flag*/
-				strcat(path, argv[i]);
-				strcpy(prog[0], path);
-				execv(path, prog);
-				perror("Error en v\n");
-				exit(EXIT_FAILURE);
-			}
-			else if (var > 0){
-				wait(&status);
-				if (i < flag-1){
-					var = fork();
-				}
-			}
-			else {
-				printf("Error haciendo fork\n");
-				exit(EXIT_FAILURE);
-			}
-		}
-		for( i = 0; i < COMMANDS - 1; i++){
-			free(prog[i]);
-		}
-		free(prog);
-	}
-	else if(strcmp(argv[flag], "-vp") == 0) { /*Asumimos que el flag del exec va a 
-										ser el último argumento*/
+        for(i = 1; i < flag; i++){
+            if(var == 0) { /*Si es un hijo, ejecuta el exec del flag*/
+                strcat(path, argv[i]);
+                strcpy(prog[0], path);
+                execv(path, prog);
+                perror("Error en v\n");
+                exit(EXIT_FAILURE);
+            }
+            else if (var > 0){
+                wait(&status);
+                if (i < flag-1){
+                    var = fork();
+                }
+            }
+            else {
+                printf("Error haciendo fork\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        for( i = 0; i < COMMANDS - 1; i++){
+            free(prog[i]);
+        }
+        free(prog);
+    }
+    else if(strcmp(argv[flag], "-vp") == 0) { /*Asumimos que el flag del exec va a 
+                                        ser el último argumento*/
 
-		var = fork();
+        var = fork();
 
-		prog = (char **)malloc(sizeof(char*) * COMMANDS); /*array de strings*/
+        prog = (char **)malloc(sizeof(char*) * COMMANDS); /*array de strings*/
 
-		for( i = 0; i < COMMANDS - 1; i++){
-			prog[i] = (char *)malloc(sizeof(char) * COMMANDSIZE);
-		}
-		prog[COMMANDS - 1] = NULL; /*El ultimo tiene que ser NULL*/
+        for( i = 0; i < COMMANDS - 1; i++){
+            prog[i] = (char *)malloc(sizeof(char) * COMMANDSIZE);
+        }
+        prog[COMMANDS - 1] = NULL; /*El ultimo tiene que ser NULL*/
 
-		for(i = 1; i < flag; i++){
-			if(var == 0) { /*Si es un hijo, ejecuta el exec del flag*/
-				strcpy(prog[0], argv[i]);
-				execvp(argv[i], prog);
-				perror("Error en vp\n");
-				exit(EXIT_FAILURE);
-			}
-			else if (var > 0){
-				wait(&status);
-				if(i < flag - 1){
-					var = fork();
-				}
-			}
-			else {
-				printf("Error haciendo fork\n");
-				exit(EXIT_FAILURE);
-			}
-		}
+        for(i = 1; i < flag; i++){
+            if(var == 0) { /*Si es un hijo, ejecuta el exec del flag*/
+                strcpy(prog[0], argv[i]);
+                execvp(argv[i], prog);
+                perror("Error en vp\n");
+                exit(EXIT_FAILURE);
+            }
+            else if (var > 0){
+                wait(&status);
+                if(i < flag - 1){
+                    var = fork();
+                }
+            }
+            else {
+                printf("Error haciendo fork\n");
+                exit(EXIT_FAILURE);
+            }
+        }
 
-	}
-	else {
-		printf("No se ha reconocido la entrada. Pruebe con: ./<ejecutable> [orden] ... [flag]");
-		exit(EXIT_FAILURE);
-		return -1;
-	}
-	wait(&status);
-	exit(EXIT_SUCCESS);
-	return 0;
+    }
+    else {
+        printf("No se ha reconocido la entrada. Pruebe con: ./<ejecutable> [orden] ... [flag]");
+        exit(EXIT_FAILURE);
+        return -1;
+    }
+    wait(&status);
+    exit(EXIT_SUCCESS);
+    return 0;
 }
---
+++ Practica1/ejercicio9.c
@@ -0,0 +1,119 @@
+/**
+* @file ejercicio8.c
+* @author Jose Ignacio Garcia, Óscar Gómez
+* @date 26-02-2017
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+#define LECTURA 0
+#define ESCRITURA 1
+
+typedef enum {
+	SUMA = 0, RESTA, PRODUCTO, COCIENTE,
+} Operaciones;
+
+int main() {
+	pid_t pchild;
+	int op1, op2, resultado, i; 
+	int a[2], b[2], pipe_status, nbytes; /*El pipe a va de padre a hijo*/
+	char *string = "20,40\n";
+	char readbuffer[120], childbuffer[120];
+	char *tok1 = NULL, *tok2 = NULL;
+	int status;
+
+	/*pipe_status = pipe(a);
+	if(pipe_status == -1) {
+		perror("Error de pipe a\n");
+		exit(EXIT_FAILURE);	
+	}
+
+	pipe_status = pipe(b);
+	if(pipe_status == -1) {
+		perror("Error de pipe b\n");
+		exit(EXIT_FAILURE);	
+	}
+
+	if((pchild = fork()) == -1) {
+		perror("fork error");
+		exit(EXIT_FAILURE);
+	}*/
+
+	for(i = 0; i < 4; i++){
+		pipe_status = pipe(a);
+		if(pipe_status == -1) {
+			perror("Error de pipe a\n");
+			exit(EXIT_FAILURE);	
+		}
+
+		pipe_status = pipe(b);
+		if(pipe_status == -1) {
+			perror("Error de pipe b\n");
+			exit(EXIT_FAILURE);	
+		}
+
+		if((pchild = fork()) == -1) {
+			perror("fork error");
+			exit(EXIT_FAILURE);
+		}
+		if(pchild > 0) {
+			close(a[0]); /*Para escribir*/
+			close(b[1]); /*Para leer*/
+			write(a[1], string, strlen(string));
+			wait(&status);
+			strcpy(readbuffer, "");
+			nbytes = read(b[0], readbuffer, sizeof(readbuffer));
+			printf("%s", readbuffer);
+			if(i < 3){
+				//pchild = fork();
+			}
+		}
+		if(pchild == 0){
+			close(a[1]); /*Para leer*/
+			close(b[0]); /*Para escribir*/
+			nbytes = read(a[0], readbuffer, sizeof(readbuffer));
+
+/*printf("read: %s\n", readbuffer);*/
+
+			tok1 = strtok(readbuffer, ", ");
+			tok2 = strtok(NULL, ", ");
+
+			strcpy(childbuffer, "");
+
+
+			op1 = atoi(tok1);
+			op2 = atoi(tok2);
+
+			if(i == SUMA){
+				resultado = op1 + op2;
+				sprintf(childbuffer, "Datos enviados a través de la tubería por el proceso PID= %d\nOperando 1: %d. Operando 2: %d. Suma: %d\n", getpid(), op1, op2, resultado);
+			}
+			else if(i == RESTA){
+				resultado = op1 - op2;
+
+				sprintf(childbuffer, "Datos enviados a través de la tubería por el proceso PID= %d\nOperando 1: %d. Operando 2: %d. Resta: %d\n", getpid(), op1, op2, resultado);
+			}
+			else if(i == PRODUCTO){
+				resultado = op1 * op2;
+
+				sprintf(childbuffer, "Datos enviados a través de la tubería por el proceso PID= %d\nOperando 1: %d. Operando 2: %d. Producto: %d\n", getpid(), op1, op2, resultado);
+			}
+			else if(i == COCIENTE){
+				resultado = op1 / op2;
+
+				sprintf(childbuffer, "Datos enviados a través de la tubería por el proceso PID= %d\nOperando 1: %d. Operando 2: %d. Cociente: %d\n", getpid(), op1, op2, resultado);
+			}
+			write(b[1], childbuffer, strlen(childbuffer));
+			exit(EXIT_SUCCESS);
+		}
+	}
+		
+	exit(EXIT_SUCCESS);
+	return 0;
+}
