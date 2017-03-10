411a3a1455a938aae54cfa6cee9722eda93ad044
OscarGB <oscar.gomez.borz@gmail.com>
2017-03-10 16:28:43 +0100

    6 terminado

--- Practica2/ejercicio6.c
+++ Practica2/ejercicio6.c
@@ -1,5 +1,12 @@
+/**
+* @file ejercicio6.c
+* @author Jose Ignacio Garcia, Óscar Gómez
+* @date 10-03-2017
+*/
+
 #include <stdio.h>
 #include <signal.h>
+#include <unistd.h>
 
 int main(){
 	int pid, status;
@@ -7,11 +14,11 @@ int main(){
 	pid = fork();
 
 	while(pid == 0){
-		printf("Soy el proceso hijo con PID: %d", getpid());
+		printf("Soy el proceso hijo con PID: %d\n", getpid());
 		sleep(5);
 	}
 
 	sleep(30);
 	status = kill(pid, SIGKILL);
-	return 0
+	return 0;
 }
